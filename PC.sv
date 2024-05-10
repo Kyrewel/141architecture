326ff69b3a10121399cb59baece281271949a12c
George H Wu <ghwu@UCSD.EDU>
2024-05-10 16:56:30 -0700

    updated pc

--- PC.sv
+++ PC.sv
@@ -4,7 +4,6 @@
 module PC #(parameter D=12)(
   input reset,					// synchronous reset
         clk,
-		reljump_en,             // rel. jump enable
         absjump_en,				// abs. jump enable
   input       [D-1:0] target,	// how far/where to jump
   output logic[D-1:0] prog_ctr
@@ -12,12 +11,10 @@ module PC #(parameter D=12)(
 
   always_ff @(posedge clk)
     if(reset)
-	  prog_ctr <= '0;
-	else if(reljump_en)
-	  prog_ctr <= prog_ctr + target;
+	    prog_ctr <= '0;
     else if(absjump_en)
-	  prog_ctr <= target;
-	else
-	  prog_ctr <= prog_ctr + 'b1;
+      prog_ctr <= target;
+    else
+      prog_ctr <= prog_ctr + 'b1;
 
 endmodule
