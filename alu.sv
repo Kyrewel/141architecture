b2237aca9fcc8f8c47595de1355de8c9a9e8f6dd
George H Wu <ghwu@UCSD.EDU>
2024-05-10 16:48:57 -0700

    updated alu

--- alu.sv
+++ alu.sv
@@ -1,42 +1,31 @@
 // combinational -- no clock
-// sample -- change as desired
 module alu(
-  input[2:0] alu_cmd,    // ALU instructions
-  input[7:0] inA, inB,	 // 8-bit wide data path
-  input      sc_i,       // shift_carry in
-  output logic[7:0] rslt,
-  output logic sc_o,     // shift_carry out
-               pari,     // reduction XOR (output)
-			   zero      // NOR (output)
+  input[3:0] alu_command,         // ALU instructions
+  input[7:0] input_A, input_B,	  // 8-bit wide data path
+  input shiftcarry_in,            // carry in bit
+  output logic[7:0] result,       // output result
+  output logic shiftcarry_out     // carry out bit
 );
 
 always_comb begin 
   rslt = 'b0;            
-  sc_o = 'b0;    
-  zero = !rslt;
-  pari = ^rslt;
-  case(alu_cmd)
-    3'b000: // add 2 8-bit unsigned; automatically makes carry-out
-      {sc_o,rslt} = inA + inB + sc_i;
-	3'b001: // left_shift
-	  {sc_o,rslt} = {inA, sc_i};
-      /*begin
-		rslt[7:1] = ina[6:0];
-		rslt[0]   = sc_i;
-		sc_o      = ina[7];
-      end*/
-    3'b010: // right shift (alternative syntax -- works like left shift
-	  {rslt,sc_o} = {sc_i,inA};
-    3'b011: // bitwise XOR
-	  rslt = inA ^ inB;
-	3'b100: // bitwise AND (mask)
-	  rslt = inA & inB;
-	3'b101: // left rotate
-	  rslt = {inA[6:0],inA[7]};
-	3'b110: // subtract
-	  {sc_o,rslt} = inA - inB + sc_i;
-	3'b111: // pass A
-	  rslt = inA;
+  case(alu_command)
+    4'b0000: // bitwise AND
+      result = input_A & input_B
+	  4'b0001: // bitwise XOR
+      result = input_A ^ input_B
+	  4'b0010: // bitwise OR
+      result = input_A | input_B
+	  4'b0011: // logical left shift
+      {shiftcarry_out, result} = {input_A, shiftcarry_in};
+	  4'b0100: // logical right shift
+      {result, shiftcarry_out} = {shiftcarry_in,input_A};
+	  4'b0101: // logical add
+      {shiftcarry_out, reslt} = input_A + input_B + shiftcarry_in
+	  4'b0110: // logical subtract
+      {shiftcarry_out, reslt} = input_A - input_B + shiftcarry_in
+    4'b0111: // pass A
+      result = input_A
   endcase
 end
    
