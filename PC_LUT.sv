ad676f952da01ecebac94c9c50760f87f547782e
Kyrewel <122554370+Kyrewel@users.noreply.github.com>
2024-06-02 19:47:09 -0700

    updated PC_LUT

--- PC_LUT.sv
+++ PC_LUT.sv
@@ -1,14 +1,14 @@
 module PC_LUT(
-  input[7:0] tag,	   // target 4 values
-  output logic[11:0] target
+  input [7:0] tag,       // target 4 values
+  output logic [11:0] target
 );
 
-logic[11:0] tagToTarget[2**12];
+logic [11:0] tagToTarget[2**12]; // Memory array for 4096 entries of 12 bits each
 
 initial begin
-	$readmemb("tag_to_instruction_line.txt", tagToTarget);
+    $readmemb("tag_to_instruction_line.txt", tagToTarget); // Load data into memory
 end
 
-always_comb target = tagToTarget[tag];
+always_comb target = tagToTarget[tag]; // Assign the output based on the tag input
 
 endmodule
--- PC_LUT_tb.sv
+++ PC_LUT_tb.sv
@@ -3,23 +3,31 @@
 module PC_LUT_tb;
 
   // Inputs
-  reg [7:0] tag,
+  reg [7:0] tag;
 
   // Outputs
   wire [11:0] target;
 
   // Instantiate the PC_LUT module
-  PC_LUT #(.D(D)) uut (
+  PC_LUT uut (
     .tag(tag),
     .target(target)
   );
 
   // Test sequence
   initial begin
+    #10;
 
     tag = 1;
+
+    #10;
+
+    $display("tag = %b", target);
     if (target == 'b000000000000) begin
       $display("Test Case 1 Passed");
+    end
+    else begin
+      $display("Test Case 1 Failed");
     end
 
     $finish;
