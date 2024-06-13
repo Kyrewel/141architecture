ba9eaca6c6ec670a29eeac08f6d9a87857d33a40
Kyrewel <122554370+Kyrewel@users.noreply.github.com>
2024-06-12 17:31:43 -0700

    lol

--- testbenches/test_bench1.sv
+++ testbenches/test_bench1.sv
@@ -40,19 +40,19 @@ initial begin
     #100ns;
     Min = 'd16;						         // start test bench Min at max value
     Max = 'd0;						         // start test bench Max at min value
-    $readmemb("test0.txt",D1.dm.core);
-    // case(loop_ct)
-    //   0: 
-	  //   1: $readmemb("tests/test1.txt",D1.dm.core);
-    //   2: $readmemb("tests/test2.txt",D1.dm.core);
-	  //   3: $readmemb("tests/test3.txt",D1.dm.core);
-    //   4: $readmemb("tests/test4.txt",D1.dm.core);
-    //   5: $readmemb("tests/test5.txt",D1.dm.core);
-    //   6: $readmemb("tests/test6.txt",D1.dm.core);
-	  //   7: $readmemb("tests/test7.txt",D1.dm.core);
-    //   8: $readmemb("tests/test8.txt",D1.dm.core);
-    //   9: $readmemb("tests/test9.txt",D1.dm.core);
-    // endcase
+  
+    case(loop_ct)
+      0: $readmemb("test0.txt",D1.dm.core);
+	    1: $readmemb("test1.txt",D1.dm.core);
+      2: $readmemb("test2.txt",D1.dm.core);
+	    3: $readmemb("test3.txt",D1.dm.core);
+      4: $readmemb("test4.txt",D1.dm.core);
+      5: $readmemb("test5.txt",D1.dm.core);
+      6: $readmemb("test6.txt",D1.dm.core);
+	    7: $readmemb("test7.txt",D1.dm.core);
+      8: $readmemb("test8.txt",D1.dm.core);
+      9: $readmemb("test9.txt",D1.dm.core);
+    endcase
     for(int i=0; i<32; i++) begin
       Tmp[i] = {D1.dm.core[2*i],D1.dm.core[2*i+1]};
       $display("%d:  %b",i,Tmp[i]);
